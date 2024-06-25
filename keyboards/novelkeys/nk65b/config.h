a35f2c8190ee2a4c7b3cbcdc054c1068dce5d8a6
Griko Nibras <griko@nibras.co>
2024-06-26 00:58:14 +0700

    feat(nk65b): both shift toggles caps word

--- keyboards/novelkeys/nk65b/config.h
+++ keyboards/novelkeys/nk65b/config.h
@@ -23,4 +23,5 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define WS2812_PWM_DMA_STREAM STM32_DMA1_STREAM3
 #define WS2812_PWM_DMA_CHANNEL 3
 
+#define BOTH_SHIFTS_TURNS_ON_CAPS_WORD 1
 #define GRAVE_ESC_GUI_OVERRIDE 1
